b82895f5d108d2320cf132548c8f615e5428068a
Sophia Gomez <sophiag28099@gmail.com>
2022-10-14 20:42:53 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -2,26 +2,30 @@
 import java.io.IOException;
 import java.net.URI;
 
-class Handler implements URLHandler {
+class Searching implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     String pencil = "";
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Searches so far: %s", pencil);
-        } else if (url.getPath().contains("/add")) {
+            return String.format("Your String Searcher tracker to Go!");
+        } 
+        
+        else if (url.getPath().contains("/add")) {
             String[] parameters = url.getQuery().split("=");
-                    pencil += (parameters[1]);
-            return ("");
+                if (parameters[1].equals("app")){
+                    return String.format("Searches so far: %s", pencil);
+                }
+
+                pencil += (parameters[1] + ", ");
+                return String.format("Okay Searched!");
+
+            
             
         } 
         
         else {
-            System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
-                
-            }
             return "404 Not Found!";
         }
     }
@@ -34,5 +38,14 @@ class Handler implements URLHandler {
 
 
 class SearchEngine {
-    
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Searching());
+    }
 }
